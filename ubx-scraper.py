052fa6f1a13ba5f04e98fe2fa60b867d5d03f7d4
melzreal <melissa.real@hotmail.com>
2020-12-30 18:36:28 +0000

    Full script

--- ubx-scraper.py
+++ ubx-scraper.py
@@ -14,54 +14,83 @@ tag_group = []
 authors_text = []
 blog_contents = []
 post_images = []
+post_images_alt_descriptions = []
 
-blog_posts = browser.find_elements_by_css_selector("a.blog-tile.blog-large-tile")
-# len(blog_posts)
-for i in range(4):
 
-    posts = browser.find_elements_by_css_selector("a.blog-tile.blog-large-tile")
-    posts[i].click()
+def loop_through_posts():
+    blog_posts = browser.find_elements_by_css_selector("a.blog-tile.blog-large-tile")
 
-    # title = browser.find_element_by_css_selector("span.blog-header__article-title")
-    # titles_text += [title.text]
+    for i in range(len(blog_posts)):
 
-    # published_date = browser.find_element_by_css_selector("div.blog-header__published-date")
-    # published_dates += [published_date.text]
+        global titles_text
+        global published_dates
+        global tag_group
+        global authors_text
+        global post_images
+        global post_images_alt_descriptions
+        global blog_contents
 
-    # tags = browser.find_elements_by_css_selector("a.blog-tags__tag.blog-tags__tag--active")
-    # tag_group += [[t.text for t in tags]]
+        posts = browser.find_elements_by_css_selector("a.blog-tile.blog-large-tile")
+        posts[i].click()
 
-    try:
-        author = browser.find_element_by_css_selector("a.blog-header__author--linked")
-        authors_text += [author.text]
-    except:
-        author = browser.find_element_by_css_selector("div.blog-header__author")
-        authors_text += [author.text]
+        title = browser.find_element_by_css_selector("span.blog-header__article-title")
 
-    images = browser.find_elements_by_css_selector(".blog-content img")
-    print("images", images)
+        titles_text += [title.text]
 
-    for image in images:
-        post_images += [image.get_attribute('src'), image.get_attribute('alt')]
+        published_date = browser.find_element_by_css_selector(
+            "div.blog-header__published-date"
+        )
 
-    blog_content = browser.find_element_by_css_selector("article.blog-content")
-    blog_contents += [blog_content.text]
+        published_dates += [published_date.text]
 
-    browser.back()
+        tags = browser.find_elements_by_css_selector(
+            "a.blog-tags__tag.blog-tags__tag--active"
+        )
 
-    # scraping blog index
+        tag_group += [[t.text for t in tags]]
 
-    # authors = browser.find_elements_by_css_selector("div.blog-large-tile__author")
-    # authors_text += [t.text for t in authors]
+        try:
+            author = browser.find_element_by_css_selector(
+                "a.blog-header__author--linked"
+            )
+            authors_text += [author.text]
+        except:
+            author = browser.find_element_by_css_selector("div.blog-header__author")
+            authors_text += [author.text]
 
-    # next_page = browser.find_element_by_css_selector("a.blog-pagination__link")
-    # next_page.click()
+        images = browser.find_elements_by_css_selector(".blog-content > p img")
+        post_images += [[t.get_attribute("src") for t in images]]
+        post_images_alt_descriptions += [[t.get_attribute("alt") for t in images]]
 
-print(post_images)
+        blog_content = browser.find_element_by_css_selector("article.blog-content")
+        blog_contents += [blog_content.text]
 
-# csv_clean_blog = [i.replace('\n','|') for i in  blog_contents]
+        browser.back()
 
-# dict = {'title': titles_text, 'date': published_dates, 'author': authors_text, 'tags': tag_group, 'content': csv_clean_blog }
+        # scraping blog index
+        # authors = browser.find_elements_by_css_selector("div.blog-large-tile__author")
+        # authors_text += [t.text for t in authors]
+        # next_page = browser.find_element_by_css_selector("a.blog-pagination__link")
+        # next_page.click()
 
-# df = pd.DataFrame(dict)
-# df.to_csv('blogs.csv')
+
+for i in range(6):
+    loop_through_posts()
+    next_page = browser.find_element_by_css_selector("a.blog-pagination__link")
+    next_page.click()
+
+
+csv_clean_blog = [i.replace("\n", "|") for i in blog_contents]
+
+dict = {
+    "title": titles_text,
+    "date": published_dates,
+    "author": authors_text,
+    "tags": tag_group,
+    "content": csv_clean_blog,
+    "images": post_images,
+    "image descriptions": post_images_alt_descriptions,
+}
+
+df = pd.DataFrame(dict)
+df.to_csv("six_blog_pages.csv")
